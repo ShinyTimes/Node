4a0af5e956b62d7c578764372cc542f9fa3b175f
ShinyTimes <andr4666@stud.kea.dk>
2017-09-25 14:22:49 +0200

    Initial Commit

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
-const path = __dirname;
+app.use(express.static(__dirname + '/wwwroot'));
+
+var options = { root: __dirname + '/wwwroot'}
 
 app.listen(5000, function(){
     console.log('Listening on port 5000!')
 });
 
 app.get('/keaevents', function(req, res){
-    res.sendFile(path + '/keaevents.html', function(err){});
+    res.sendFile('/keaevents.html', options);
 });
+
+app.get('/', function(req, res){
+    res.sendFile('/keaevents.html', options);
+});
+
+app.get('/about', function(req, res){
+    res.sendFile('/about.html', options);
+});
+
